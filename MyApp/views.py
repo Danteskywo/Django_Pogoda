c96bed2753999657151a02413dffcce79117c426
Пестерев Илья Георгиевич <279017025@ACADEMY.TOP>
2025-10-01 21:06:08 +0300

    CSS

--- MyApp/views.py
+++ MyApp/views.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -9,13 +10,14 @@ from .forms import CityForm
 
 # Create your views here.
 def index(request):
-	appid = '089b41837623a98e1ad261c4b10ee69b'
+	appid = os.environ.get('OPENWEATHER_API_KEY', '089b41837623a98e1ad261c4b10ee69b')
 	# appid = os.environ['appid']
-	url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+	url_template = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
 	if (request.method == 'POST'):
 		form = CityForm(request.POST)
-		form.save()
+		if form.is_valid():
+			form.save()
 
 	form = CityForm()
 
@@ -23,15 +25,16 @@ def index(request):
 	all_cities = []
 
 	for city in cities:
-		res = requests.get(url.format(city.name)).json()
+		res = requests.get(url_template.format(city.name)).json()
 		city_info = {
 			'city': city.name,
 			'temp': res["main"]["temp"],
 			'icon': res["weather"][0]["icon"]
 		}
+
 		all_cities.append(city_info)
 
 
 	context = {'all_info': all_cities, 'form': form} 	 # Передача данных в html шаблон
 
-	return render(request, 'weather/index.html', context)
+	return render(request, 'index.html', context)
