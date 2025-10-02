62c0b913cf0fa51bf03b89a59d7e90ab225b64e9
Danteskywo <skywo@mail.ru>
2025-10-02 22:13:59 +0300

    СSS+views_work

--- MyApp/views.py
+++ MyApp/views.py
@@ -1,4 +1,3 @@
-import os
 import requests
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -7,12 +6,11 @@ from .forms import CityForm
 
 
 
-
 # Create your views here.
 def index(request):
-	appid = os.environ.get('OPENWEATHER_API_KEY', '089b41837623a98e1ad261c4b10ee69b')
+	appid = '089b41837623a98e1ad261c4b10ee69b'
 	# appid = os.environ['appid']
-	url_template = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+	url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
 	if (request.method == 'POST'):
 		form = CityForm(request.POST)
@@ -25,14 +23,22 @@ def index(request):
 	all_cities = []
 
 	for city in cities:
-		res = requests.get(url_template.format(city.name)).json()
-		city_info = {
-			'city': city.name,
-			'temp': res["main"]["temp"],
-			'icon': res["weather"][0]["icon"]
-		}
-
-		all_cities.append(city_info)
+		res = requests.get(url.format(city.name)).json()
+		if res.get('cod') == 200:
+			city_info = {
+				'city': city.name,
+				'temp': res["main"]["temp"],
+				'icon': res["weather"][0]["icon"],
+			}
+		else:
+        # В случае ошибки можно указать, что данные недоступны
+			city_info = {
+				'city': city.name,
+				'temp': 'N/A',
+				'icon': 'error_icon.png',  # или оставить пустым
+				'error_message': res.get('message', 'Не удается получить погоду')
+			}
+	all_cities.append(city_info)
 
 
 	context = {'all_info': all_cities, 'form': form} 	 # Передача данных в html шаблон
